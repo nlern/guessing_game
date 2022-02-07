902d06281a8581de61c60e25f9dc62bd6dbc3968
Shantanu Dutta <connect.shantanu.dutta@gmail.com>
2022-02-07 07:37:52 +0530

    feat: handled non numeric guess errors

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,30 @@ fn main() {
     println!("Guessing game!");
 
     let secret_number: u32 = rand::thread_rng().gen_range(1..101);
-
     println!("The secret number is: {}", secret_number);
+    loop {
+        println!("Please input your guess.");
+        
+        let mut guess = String::new();
 
-    println!("Please input your guess.");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+            let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("You guessed: {}", guess);
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
